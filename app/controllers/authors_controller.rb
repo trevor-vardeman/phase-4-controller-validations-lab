08f19ff71c017da4b19e291a4c2cfe48d98fbf7c
Trevor Vardeman <trevortx@gmail.com>
2022-08-13 21:51:59 -0700

    commit tree d91076b52367a8e862950ea463f9e5da5d020933

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  
+
   def show
     author = Author.find(params[:id])
 
@@ -8,14 +8,17 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
-    render json: author, status: :created
+    if author.valid?
+      render json: author, status: :created
+    else
+      render json: { errors: author.errors }, status: :unprocessable_entity
+    end
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
 end
